7e47345650a3c6b58081f9a674d164a1e143b7e8
nchirkov <chirkov.nikita@gmail.com>
2016-10-27 00:46:47 +0300

    Fix style and routing

--- config.js
+++ config.js
@@ -1,48 +1,49 @@
-mailBoxApp.config(function($stateProvider) {
-    $stateProvider.state('home', {
-            url: '',
-            component: 'home',
-        })
-        .state('home.mailbox', {
-            url: '/mailbox/{mailboxId}',
-            component: 'mailbox',
-            resolve: {
-                mailbox: function(MailService, $stateParams) {
-                    return MailService.getBox($stateParams.mailboxId);
+mailBoxApp.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+                url: '/',
+                component: 'home',
+            })
+            .state('home.mailbox', {
+                url: 'mailbox/{mailboxId}',
+                component: 'mailbox',
+                resolve: {
+                    mailbox: function(MailService, $stateParams) {
+                        return MailService.getBox($stateParams.mailboxId);
+                    }
                 }
-            }
-        })
-        .state('home.mailbox.mail', {
-            url: '/{mailId}',
-            component: 'mail',
-            resolve: {
-                mail: function(mailbox, $stateParams) {
-                    return mailbox.find(function(mail) {
-                        return mail._id === $stateParams.mailId;
-                    })
+            })
+            .state('home.mailbox.mail', {
+                url: '/{mailId}',
+                component: 'mail',
+                resolve: {
+                    mail: function(mailbox, $stateParams) {
+                        return mailbox.find(function(mail) {
+                            return mail._id === $stateParams.mailId;
+                        })
+                    }
                 }
-            }
-        })
-        .state('home.usercard', {
-            url: '/users/{userId}',
-            component: 'usercard',
-            resolve: {
-                user: function(UserService, $stateParams) {
-                    return UserService.getUser($stateParams.userId);
+            })
+            .state('home.usercard', {
+                url: 'users/{userId}',
+                component: 'usercard',
+                resolve: {
+                    user: function(UserService, $stateParams) {
+                        return UserService.getUser($stateParams.userId);
+                    }
                 }
-            }
-        })
-        .state('login', {
-            url: '/login',
-            component: 'login'
-        })
-})
-.run(($transitions, AuthService) => {
-    $transitions.onEnter({
-        to: '**'
-    }, function($transition$, $state$) {
-        if ($state$.name !== 'login' && !AuthService.isAuthenticated()) {
-            return $transition$.router.stateService.target('login');
-        }
+            })
+            .state('login', {
+                url: '/login',
+                component: 'login'
+            })
+        $urlRouterProvider.otherwise('/');
     })
-})
+    .run(($transitions, AuthService) => {
+        $transitions.onEnter({
+            to: '**'
+        }, function($transition$, $state$) {
+            if ($state$.name !== 'login' && !AuthService.isAuthenticated()) {
+                return $transition$.router.stateService.target('login');
+            }
+        })
+    })
