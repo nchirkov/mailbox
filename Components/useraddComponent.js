e0a0ddebfeaa024e99dbaeacb3a6bb1276e73c7f
nchirkov <chirkov.nikita@gmail.com>
2016-11-05 19:47:50 +0300

    Use user input for add usercard

--- Components/useraddComponent.js
+++ Components/useraddComponent.js
@@ -2,11 +2,11 @@ mailBoxApp.component('useradd', {
     controller: function(UserService, $state) {
         this.addUser = () => {
             let user = {
-                "fullName": "Тест Тестов",
-                "email": "natalya@mail.ru",
-                "birthdate": "1996-10-10",
-                "gender": "F",
-                "address": "ул. Лермонтова, 59"
+                "fullName": this.fullName,
+                "email": this.email,
+                "birthdate": this.birthdate,
+                "gender": this.gender,
+                "address": this.address
             };
             UserService.addUser(user).then(() =>
                 $state.go('home', {}, {
