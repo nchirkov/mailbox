e73205741c02d114e16ff90b3ec55095f9f3c454
nchirkov <chirkov.nikita@gmail.com>
2016-11-08 00:25:01 +0300

    Define baseurl variable

--- users/user.service.js
+++ users/user.service.js
@@ -1,26 +1,28 @@
 mailBoxApp.service('UserService', function($http) {
+    let usersBaseUrl = 'https://test-api.javascript.ru/v1/nchirkov/users/';
+
     this.getUsers = () => {
-        return $http.get('https://test-api.javascript.ru/v1/nchirkov/users')
+        return $http.get(usersBaseUrl)
             .then(response => response.data);
     }
 
     this.getUser = (id) => {
-        return $http.get('https://test-api.javascript.ru/v1/nchirkov/users/' + id)
+        return $http.get(usersBaseUrl + id)
             .then(response => response.data)
     }
 
     this.addUser = (user) => {
-        return $http.post('https://test-api.javascript.ru/v1/nchirkov/users/', user)
+        return $http.post(usersBaseUrl, user)
             .then(response => response.data)
     }
 
     this.updateUser = (user) => {
-        return $http.patch('https://test-api.javascript.ru/v1/nchirkov/users/' + user._id, user)
+        return $http.patch(usersBaseUrl + user._id, user)
             .then(response => response.data)
     }
 
     this.deleteUser = (id) => {
-        return $http.delete('https://test-api.javascript.ru/v1/nchirkov/users/' + id)
+        return $http.delete(usersBaseUrl + id)
             .then(response => response.data)
     }
 })
